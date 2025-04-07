1fce09599fc931741b0ff46e3a973da99a032dca
Samrajya Bhari <cloudsamrajya@gmail.com>
2025-04-07 21:58:07 +0545

    ui of food section

--- Backend/restaurant.php
+++ Backend/restaurant.php
@@ -12,11 +12,11 @@ $restaurant_food_item = $stmt -> fetchAll();
 ?>
 <div class="table-container">
     <table class="tbl" >
-        <tr>
-            <th>
+        <tr class="no-hover">
+            <th >
                 Food Item
             </th>
-            <th>
+            <th >
                 Price
             </th>
             <th>
@@ -26,15 +26,15 @@ $restaurant_food_item = $stmt -> fetchAll();
                 Add
             </th>
         </tr>
-        <?php if (empty($restaurant_food_item)) {
-    echo "No data found!";
-}?>
+        <?php if (empty($restaurant_food_item)): ?>
+        <tr><td colspan="4" style="text-align: center; padding: 20px; color: #666;">No food items found!</td></tr>
+    <?php endif; ?>
         <?php foreach($restaurant_food_item as $r_f_i): ?>
         <tr>
             <td><?php echo $r_f_i['name']?></td>
             <td><?php echo $r_f_i['price'] ?></td>
-            <td><?php echo $r_f_i['availability'] ?></td>
-            <td>+</td>
+            <td><span style="color: <?php echo ($r_f_i['availability']=='Available') ? '#2ecc71':'#e74c3c' ?> "><?php echo $r_f_i['availability'] ?></span>  </td>
+            <td onclick="cart.php"><i class="bi bi-plus-lg"></i></td> <!--- yeta bata suru -->
         </tr>
         <?php endforeach ?>
         
